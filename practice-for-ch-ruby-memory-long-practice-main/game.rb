660af379ce4ef9b51a57f392586675bc2488ae56
Justin Aitken <76704172+Cuponk@users.noreply.github.com>
2023-06-06 15:31:36 -0700

    add little_won and won

--- practice-for-ch-ruby-memory-long-practice-main/game.rb
+++ practice-for-ch-ruby-memory-long-practice-main/game.rb
@@ -1,5 +1,19 @@
 class Game
     def initialize
-        
-    end    
+        @board = Board.new
+        @card = Card.new
+    end
+
+    def game_over?
+        @board.won?
+    end
+
+    def print
+        @board.render
+    end
+
+    def show_pos
+        @first = first
+        @second = second
+    end
 end
