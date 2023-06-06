9bf5ab246a3bdf2aab808eddeb966ffdccad99e3
farivar <farivaramiri@gmail.com>
2023-06-06 14:23:39 -0700

    2nd

--- practice-for-ch-ruby-memory-long-practice-main/card.rb
+++ practice-for-ch-ruby-memory-long-practice-main/card.rb
@@ -1,6 +1,12 @@
 class Card
     attr_reader :cards
+
     def initialize
         @cards = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
     end
+
+    def hide
+        Array.new(4) {Array.new(4)}
+    end
+    
 end
