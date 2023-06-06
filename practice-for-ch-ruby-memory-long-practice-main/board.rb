48259a4b0269583dbc1b24ed38574e078b93ce86
Justin Aitken <76704172+Cuponk@users.noreply.github.com>
2023-06-06 13:58:19 -0700

    setup classes

--- practice-for-ch-ruby-memory-long-practice-main/board.rb
+++ practice-for-ch-ruby-memory-long-practice-main/board.rb
@@ -1,5 +1,39 @@
 class Board
     def initialize
-        
+        @board = Array.new(4) {Array.new(4)}
+        @populate = []
+        @player = Player.new
+        @ai = AI.new
+    end 
+
+    def [](pos)
+        @board[pos[0]][pos[1]]
     end
+
+    def []=(pos, val)
+        @board[pos[0]][pos[1]] = val
+    end
+
+    def populate
+        row = rand(0...@board.length)
+        col = rand(0...@board.length)
+        @populate = 
+    end
+
+    def render
+        @board.each do |row|
+            puts row
+        end
+    end
+
+    def won?
+        return true if 
+    end
+
+    def reveal(pos)
+        if self[pos] ==
+
+        end
+    end
+
 end
